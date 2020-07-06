91ea28e5e1d6f28d39727ee52284a6bebb1d172a
paulmrest <paulmrest@gmail.com>
2020-07-06 15:25:05 -0700

    StartSequence method

--- Lab01a - Exception Handling and Debugging/Program.cs
+++ Lab01a - Exception Handling and Debugging/Program.cs
@@ -6,12 +6,68 @@ namespace Lab01a___Exception_Handling_and_Debugging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encountered.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Program completed successfully.");
+            }
         }
 
         static void StartSequence()
         {
-
+            try
+            {
+                Console.WriteLine("Enter a number greater than zero.");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                int[] intArray = new int[userInput];
+                Populate(intArray);
+                int sum = GetSum(intArray);
+                int product = GetProduct(intArray, sum);
+                decimal quotient = GetQuotient(product);
+                Console.WriteLine("Your array size is: {0}", intArray.Length);
+                Console.Write("The numbers in the array are: ");
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    if (i == intArray.Length - 1)
+                    {
+                        Console.Write("{0}", intArray[i]);
+                    }
+                    else
+                    {
+                        Console.Write("{0}, ", intArray[i]);
+                    }
+                }
+                Console.WriteLine("The sum of the array is: {0}", sum);
+                Console.WriteLine("{0} * {1} = {2}", sum, product, sum * product);
+                Console.WriteLine("{0} / {1} = {2}", sum * product, quotient, (sum * product) / quotient);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("FormatException error.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("OverflowException error.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception encountered.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
         }
 
         static int[] Populate(int[] intArray)
